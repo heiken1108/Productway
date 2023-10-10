e14658fa090c57e6a5d4c9795d8d8789228de907
Jonas Rønning <jr.ronning@gmail.com>
2023-10-10 15:11:44 +0200

    (#10) Added category view and navigation support

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import AllStores from './AllProducts';
+import AllStores from './Pages/AllProducts/AllProducts';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CategoryPage from './Pages/CategoryPage/CategoryPage';
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ function App() {
 			<QueryClientProvider client={queryClient}>
 				<Routes>
 					<Route path="/" element={<AllStores />} />
+					<Route path="/category/:categoryName" element={<CategoryPage/>}/>
 				</Routes>
 			</QueryClientProvider>
 		</Router>
