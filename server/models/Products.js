a4b2e0119b6c8209dc1fb211ffb5e9546c99c7a9
Håkon Steensland Hoelsæter <haaksho@stud.ntnu.no>
2023-10-11 09:38:37 +0200

    (#6) Resolve "Populate database"

--- server/models/Products.js
+++ server/models/Products.js
@@ -22,7 +22,7 @@ const ProductSchema = new mongoose.Schema({
         required: true,
     },
     category: {
-        type: [String],
+        type: String,
         required: false,
     },
     description: {
