65c2d28ed94f70a57fa03de7c9a934e2c1372bd8
Håkon Steensland Hoelsæter <haaksho@stud.ntnu.no>
2023-10-11 13:05:09 +0200

    (#17) Created ProductCard, with navigate-support

---
+++ client/src/Components/ProductCard/ProductCard.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+import { IProduct } from '../../data/types';
+import './ProductCard.css'
+
+
+export default function ProductCard({item}: {item: IProduct}) {
+    const navigate = useNavigate();
+    
+    return (
+        <div className="productCard" onClick={() => navigate("/product/" + item.productID)}>
+            <div className='imgDiv'>
+                <img src={item.image} alt={item.name} />
+            </div>
+            <div className='textDiv'>
+                <h6 className='tittel'>{item.name}</h6>
+                <p className='prisParagraph'>Pris: {item.currentPrice} @ {item.store}</p>
+            </div>
+        </div>
+    );
+}
--- client/src/Pages/CategoryPage/CategoryPage.tsx
+++ client/src/Pages/CategoryPage/CategoryPage.tsx
@@ -1,7 +1,32 @@
+//import { useState } from 'react';
+import { IProduct } from '../../data/types';
+//import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import './CategoryPage.css';
+import ProductCard from '../../Components/ProductCard/ProductCard';
+
+const todaysItem: IProduct = {
+	//Mock data. Will be changed with data from backend
+	brand: 'Nestle',
+	category: 'Pålegg & frokost',
+	currentPrice: 54.9,
+	description:
+		'Gi hele familien en god start på dagen! \nHavre Cheerios er sprø \
+	fullkornsringer med 92% fullkorn og et høyt innhold av fiber. Produktet er også \
+	nøkkelhullsmerket.',
+	ean: '5900020037046',
+	image: 'https://bilder.ngdata.no/5900020037046/kmh/large.jpg',
+	name: 'Cheerios Havre 375g Nestle',
+	productID: 1011,
+	store: 'Joker',
+	weight: 375,
+	weightUnit: 'g',
+};
+
 
 export default function CategoryPage() {
+	const { categoryName } = useParams();
+	/*const [products, setProducts] = useState<IProduct[]>([]);
 	const { categoryName } = useParams();
 
 	return (
@@ -11,5 +36,17 @@ export default function CategoryPage() {
 				Siden for <strong>{categoryName}</strong>
 			</p>
 		</div>
+	);*/
+	return (
+		<>
+			<h1 className='title'>Siden for <strong>{categoryName}</strong></h1>
+			<div className='ProductsContainer'>
+				<ProductCard item={todaysItem} />
+				<ProductCard item={todaysItem} />
+				<ProductCard item={todaysItem} />
+				<ProductCard item={todaysItem} />
+				<ProductCard item={todaysItem} />
+			</div>
+		</>
 	);
 }
