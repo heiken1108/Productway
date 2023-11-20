12cc88a6d4854bd738ef862200ca74886bd18d5d
Jonas Rønning <jr.ronning@gmail.com>
2023-11-20 10:11:50 +0100

    (#70) Move categories-array to separate data file

--- client/src/Pages/AllProducts/AllProducts.tsx
+++ client/src/Pages/AllProducts/AllProducts.tsx
@@ -2,57 +2,10 @@ import { useNavigate } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
 
 import TodaysItem from '../../Components/TodaysItem/TodaysItem';
+import categories from '../../data/categories';
 import { categoryFilterState } from '../../store/atoms';
 
 import './AllProducts.css';
-
-const categories = [
-	{
-		key: 0,
-		name: 'Snacks & godteri',
-		icon: 'fi fi-rr-popcorn',
-	},
-	{
-		key: 1,
-		name: 'Personlige artikler',
-		icon: 'fi fi-rr-soap',
-	},
-	{
-		key: 2,
-		name: 'Kjøtt',
-		icon: 'fi fi-rr-meat',
-	},
-	{
-		key: 3,
-		name: 'Middag',
-		icon: 'fi fi-rr-restaurant',
-	},
-	{
-		key: 4,
-		name: 'Ost',
-		icon: 'fi fi-rr-cheese',
-	},
-	{
-		key: 5,
-		name: 'Dessert',
-		icon: 'fi fi-rs-pie',
-	},
-	{
-		key: 6,
-		name: 'Pålegg & frokost',
-		icon: 'fi fi-rr-bread-slice',
-	},
-	{
-		key: 7,
-		name: 'Middagstilbehør',
-		icon: 'fi fi-rr-sauce',
-	},
-	{
-		key: 8,
-		name: 'Drikke',
-		icon: 'fi fi-rr-drink-alt',
-	},
-];
 
 export default function AllStores() {
 	const navigate = useNavigate();
---
+++ client/src/data/categories.tsx
@@ -0,0 +1,49 @@
+const categories = [
+	{
+		key: 0,
+		name: 'Snacks & godteri',
+		icon: 'fi fi-rr-popcorn',
+	},
+	{
+		key: 1,
+		name: 'Personlige artikler',
+		icon: 'fi fi-rr-soap',
+	},
+	{
+		key: 2,
+		name: 'Kjøtt',
+		icon: 'fi fi-rr-meat',
+	},
+	{
+		key: 3,
+		name: 'Middag',
+		icon: 'fi fi-rr-restaurant',
+	},
+	{
+		key: 4,
+		name: 'Ost',
+		icon: 'fi fi-rr-cheese',
+	},
+	{
+		key: 5,
+		name: 'Dessert',
+		icon: 'fi fi-rs-pie',
+	},
+	{
+		key: 6,
+		name: 'Pålegg & frokost',
+		icon: 'fi fi-rr-bread-slice',
+	},
+	{
+		key: 7,
+		name: 'Middagstilbehør',
+		icon: 'fi fi-rr-sauce',
+	},
+	{
+		key: 8,
+		name: 'Drikke',
+		icon: 'fi fi-rr-drink-alt',
+	},
+];
+
+export default categories;
